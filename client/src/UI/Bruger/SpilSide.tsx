90ab946d57f86eba6eadacf211188c2adc016220
Vlady <vlazav01@easv365.dk>
2025-11-18 22:29:00 +0100

    "board colors"

--- client/src/UI/Bruger/SpilSide.tsx
+++ client/src/UI/Bruger/SpilSide.tsx
@@ -1,4 +1,5 @@
 import '../CSS/SpilSide.css'
+import {useState} from "react";
 
 export type ClickableGridUIProps ={
     activeIndices?: number[];
@@ -15,7 +16,20 @@ export default function SpilSide({
     const rows = 4;
     const cols = 4;
     const total = rows * cols;
-    const activeSet = new Set(activeIndices);
+    
+    //const activeSet = new Set(activeIndices);
+    
+    const [selectedPigeons, setSelectedPigeons] = useState<number[]>([]);
+    
+   //logic for changing colors when button is pressed
+    const togglePigeon = (index: number) => {
+        if (selectedPigeons.includes(index))
+        {
+            setSelectedPigeons(selectedPigeons.filter(pigeonId => pigeonId !== index));
+        } else{
+            setSelectedPigeons([...selectedPigeons, index]);
+        }   
+    };
 
     return (
         <div className={`cg-container ${className}`}>
@@ -26,17 +40,22 @@ export default function SpilSide({
             className="cg-grid"
             style={{gridTemplateColumns: `repeat(${cols}, 1fr)`}}
             >
-                {Array.from({length: total}).map((_, idx) => (
+                {Array.from({length: total}).map((_, idx) => {
+                    
+                    const isActive = selectedPigeons.includes(idx);
+                    
+                    return (
                     <button
                         key={idx}
                         role="gridcell"
-                        aria-selected={activeSet.has(idx)}
-                        className={`cg-cell ${activeSet.has(idx) ? "cg-cell-active" : ""}`}
-                        onClick={onCellClick ? () => onCellClick(idx) : undefined}
+                        aria-selected={isActive}
+                        className={`cg-cell ${isActive ? "cg-cell-active" : ""}`}
+                        onClick={() => togglePigeon(idx)}
                     >
                         {idx + 1}
                     </button>
-                ))}
+                    );
+                })}
             </div>
         </div>
     );
