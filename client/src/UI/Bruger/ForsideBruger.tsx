6571dfe4a6cbc31a00043d3a9f18e457f3dac258
Kimphung3105 <kimngu01@easv365.dk>
2025-11-19 08:46:27 +0100

    Routing til ForsideBruger.tsx

--- client/src/UI/Bruger/ForsideBruger.tsx
+++ client/src/UI/Bruger/ForsideBruger.tsx
@@ -1,20 +1,23 @@
+import {useNavigate} from "react-router";
 
 function ForsideBruger() {
+    const navigate = useNavigate();
+
     return (
         <div>
             <div>
-            <button>
+            <button onClick={() => navigate("/købplade")}>
                 Køb Plade
             </button>
-            <button>
+            <button onClick={() => navigate("/aktiv-spil")}>
                 Aktive Spil
             </button>
             </div>
             <div>
-                <button>
+                <button onClick={() => navigate("/spilhistorik")}>
                     Spilhistorik
                 </button>
-                <button>
+                <button onClick={() => navigate("/profil")}>
                     Profil
                 </button>
             </div>
