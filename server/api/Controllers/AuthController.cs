7425e1ab19836d10dc50b98b6da6761f095bd040
Svoldgaard <Svoldgaard96@gmail.com>
2025-11-19 10:44:20 +0100

    login

---
+++ server/api/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+using api.Models.Dtos.Requests;
+using Api.Models.Dtos.Responses;
+using Api.Security;
+using Api.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController(IAuthService service, ITokenService tokenService) : ControllerBase
+{
+    
+    [HttpPost]
+    [Route("login")]
+    [AllowAnonymous]
+    public async Task<LoginResponse> Login([FromBody] LoginRequest request)
+    {
+        var userInfo = service.Authenticate(request);
+        var token = tokenService.CreateToken(userInfo);
+        return new LoginResponse(token);
+    }
+    
+    [HttpPost]
+    [Route("logout")]
+    public async Task<IResult> Logout()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet]
+    [Route("userinfo")]
+    public async Task<AuthUserInfo?> UserInfo()
+    {
+        return service.GetUserInfo(User);
+    }
+}
---
+++ server/api/Security/NSecArgon2idPasswordHasher.cs
@@ -0,0 +1,41 @@
+using System.Security.Cryptography;
+using dataaccess.Entity;
+using Microsoft.AspNetCore.Identity;
+
+namespace Api.Security;
+
+public class NSecArgon2IdPasswordHasher : IPasswordHasher<Login>
+{
+    public string HashPassword(Login user, string password)
+    {
+        var salt = RandomNumberGenerator.GetBytes(128 / 8);
+        var hash = GenerateHash(password, salt);
+        return $"argon2id${Convert.ToBase64String(salt)}${Convert.ToBase64String(hash)}";
+    }
+
+    public PasswordVerificationResult VerifyHashedPassword(
+        Login user,
+        string hashedPassword,
+        string providedPassword
+    )
+    {
+        var parts = hashedPassword.Split('$');
+        var salt = Convert.FromBase64String(parts[1]);
+        var storedHash = Convert.FromBase64String(parts[2]);
+        var providedHash = GenerateHash(providedPassword, salt);
+        return CryptographicOperations.FixedTimeEquals(storedHash, providedHash)
+            ? PasswordVerificationResult.Success
+            : PasswordVerificationResult.Failed;
+    }
+
+    public byte[] GenerateHash(string password, byte[] salt)
+    {
+        var hashAlgo = new NSec.Cryptography.Argon2id(new NSec.Cryptography.Argon2Parameters
+        {
+            DegreeOfParallelism = 1,
+            MemorySize = 12288,
+            NumberOfPasses = 3,
+        });
+        return hashAlgo.DeriveBytes(password, salt, 128);
+    }
+}
