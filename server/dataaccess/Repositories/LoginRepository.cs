601b7c4b19c1a32713a4a3cf973219c67d80f375
Svoldgaard <Svoldgaard96@gmail.com>
2025-11-21 08:44:56 +0100

    C# part working for login

--- server/dataaccess/Repositories/LoginRepository.cs
+++ server/dataaccess/Repositories/LoginRepository.cs
@@ -1,8 +1,9 @@
 using dataaccess.Entity;
+using dataaccess.MyDbContext;
 using DataAccess.Repositories;
 using Microsoft.EntityFrameworkCore;
 
-public class LoginRepository(DbContext context) : BaseRepository<Login>(context)
+public class LoginRepository(MyDbContext context) : BaseRepository<Login>(context)
 {
     protected override DbSet<Login> Set => Context.Set<Login>();
 }
