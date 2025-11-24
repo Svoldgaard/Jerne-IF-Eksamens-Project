b68374dcfc8245a09f62eed366435f26fbf87647
NadjaBrandenstein <nadbra01@easv365.dk>
2025-11-24 15:20:36 +0100

    Spilhistorik Admin UI done

--- client/src/UI/Admin/SpilhistorikAdmin.tsx
+++ client/src/UI/Admin/SpilhistorikAdmin.tsx
@@ -1,9 +1,12 @@
-import '../CSS/SpilhistorikBruger.css'
+import '../CSS/SpilhistorikAdmin.css'
 import Logo from "../../../public/Logo.png";
 import Header from "../../Component/Header.tsx";
 import Footer from "../../Component/Footer.tsx";
+import {Accordion, AccordionSummary, Typography} from "@mui/material";
+
 
 function SpilhistorikAdmin() {
+
     return (
         <div className="page-spilhistorik-admin">
             <span className="logo-plade">
@@ -13,10 +16,69 @@ function SpilhistorikAdmin() {
                 <Header/>
                 <div className="background-spilhistorik-admin">
 
-                    <input
-                        type="text"
-                        placeholder="Spilhistorikadmin"
-                    />
+                    <Accordion disabled>
+                        <AccordionSummary aria-controls="panel2-content"
+                                          id="panel2-header">
+                            <Typography component="span"> Uge 48 2025 </Typography>
+                            <Typography component="span">15, 5, 8</Typography>
+                        </AccordionSummary>
+                    </Accordion>
+
+                    <Accordion disabled>
+                        <AccordionSummary aria-controls="panel2-content"
+                                          id="panel2-header">
+                            <Typography component="span"> Uge 47 2025 </Typography>
+                            <Typography component="span">11, 9, 1</Typography>
+                        </AccordionSummary>
+                    </Accordion>
+
+                    <Accordion disabled>
+                        <AccordionSummary aria-controls="panel2-content"
+                                          id="panel2-header">
+                            <Typography component="span"> Uge 46 2025 </Typography>
+                            <Typography component="span">10, 6, 9</Typography>
+                        </AccordionSummary>
+                    </Accordion>
+
+                    <Accordion disabled>
+                        <AccordionSummary aria-controls="panel2-content"
+                                          id="panel2-header">
+                            <Typography component="span"> Uge 45 2025 </Typography>
+                            <Typography component="span">7, 12, 15</Typography>
+                        </AccordionSummary>
+                    </Accordion>
+
+                    <Accordion disabled>
+                        <AccordionSummary aria-controls="panel2-content"
+                                          id="panel2-header">
+                            <Typography component="span"> Uge 44 2025 </Typography>
+                            <Typography component="span">2, 15, 9</Typography>
+                        </AccordionSummary>
+                    </Accordion>
+
+                    <Accordion disabled>
+                        <AccordionSummary aria-controls="panel2-content"
+                                          id="panel2-header">
+                            <Typography component="span"> Uge 43 2025 </Typography>
+                            <Typography component="span">16, 6, 13</Typography>
+                        </AccordionSummary>
+                    </Accordion>
+
+                    <Accordion disabled>
+                        <AccordionSummary aria-controls="panel2-content"
+                                          id="panel2-header">
+                            <Typography component="span"> Uge 42 2025 </Typography>
+                            <Typography component="span">6, 3, 7</Typography>
+                        </AccordionSummary>
+                    </Accordion>
+
+                    <Accordion disabled>
+                        <AccordionSummary aria-controls="panel2-content"
+                                          id="panel2-header">
+                            <Typography component="span"> Uge 41 2025 </Typography>
+                            <Typography component="span">11, 1, 9</Typography>
+                        </AccordionSummary>
+                    </Accordion>
 
                 </div>
             </div>
