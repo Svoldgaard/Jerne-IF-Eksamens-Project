ca584d0ae932748aff8c5ef1bc1a2fb94c2ba3da
Vlady <vlazav01@easv365.dk>
2025-11-25 22:33:09 +0100

    saves plade into db, still need to fix

---
+++ server/api/Models/Dtos/Request/PladeRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace api.Models.Dtos.Request;
+
+public class PladeRequest
+{
+    public List<int> SelectedNumbers { get; set; }
+    public int PriceId { get; set; }
+    public int UserId {get; set;}
+    public bool Repeat {get; set;}
+}
