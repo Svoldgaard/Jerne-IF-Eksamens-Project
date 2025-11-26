43402bc062b2ed5e70f472ad782b5ac14c1db8a2
Svoldgaard <Svoldgaard96@gmail.com>
2025-11-26 08:35:14 +0100

    logout works now

--- client/src/Component/Header.tsx
+++ client/src/Component/Header.tsx
@@ -2,15 +2,18 @@ import * as React from "react";
 import SidebarBruger from "../UI/Bruger/SidebarBruger.tsx";
 import '../UI/CSS/Header.css';
 import Logout from "../../public/Logout.png";
+import { useAuth } from "../Hooks/useAuth";
+
 
 
 const Header: React.FC = () => {
+    const { logout } = useAuth();
     return (
         <footer className="header">
             <div className="header-left">
                 <SidebarBruger/>
             </div>
-            <button className="header-right">
+            <button className="header-right" onClick={logout}>
                 <img src={Logout} alt="Logout" className="header-icon" />
             </button>
         </footer>
