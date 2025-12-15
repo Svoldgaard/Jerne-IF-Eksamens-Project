a2f38b3df02f5ed7c9349e09d35786e8eb6622e0
Vlady <vlazav01@easv365.dk>
2025-12-15 17:04:43 +0100

    commit

--- client/src/generated-ts-client.ts
+++ client/src/generated-ts-client.ts
@@ -569,6 +569,81 @@ export class PladeClient {
         }
         return Promise.resolve<FileResponse>(null as any);
     }
+
+    getAllWinningPlades(): Promise<WinningPladeResponse[]> {
+        let url_ = this.baseUrl + "/api/Plade/admin/winning-plades";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_: RequestInit = {
+            method: "GET",
+            headers: {
+                "Accept": "application/json"
+            }
+        };
+
+        return this.http.fetch(url_, options_).then((_response: Response) => {
+            return this.processGetAllWinningPlades(_response);
+        });
+    }
+
+    protected processGetAllWinningPlades(response: Response): Promise<WinningPladeResponse[]> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+            let result200: any = null;
+            result200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver) as WinningPladeResponse[];
+            return result200;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<WinningPladeResponse[]>(null as any);
+    }
+
+    updateUdbetaltStatus(request: UpdatePladeRequest): Promise<FileResponse> {
+        let url_ = this.baseUrl + "/api/Plade/admin/update-udbetalt";
+        url_ = url_.replace(/[?&]$/, "");
+
+        const content_ = JSON.stringify(request);
+
+        let options_: RequestInit = {
+            body: content_,
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/octet-stream"
+            }
+        };
+
+        return this.http.fetch(url_, options_).then((_response: Response) => {
+            return this.processUpdateUdbetaltStatus(_response);
+        });
+    }
+
+    protected processUpdateUdbetaltStatus(response: Response): Promise<FileResponse> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200 || status === 206) {
+            const contentDisposition = response.headers ? response.headers.get("content-disposition") : undefined;
+            let fileNameMatch = contentDisposition ? /filename\*=(?:(\\?['"])(.*?)\1|(?:[^\s]+'.*?')?([^;\n]*))/g.exec(contentDisposition) : undefined;
+            let fileName = fileNameMatch && fileNameMatch.length > 1 ? fileNameMatch[3] || fileNameMatch[2] : undefined;
+            if (fileName) {
+                fileName = decodeURIComponent(fileName);
+            } else {
+                fileNameMatch = contentDisposition ? /filename="?([^"]*?)"?(;|$)/g.exec(contentDisposition) : undefined;
+                fileName = fileNameMatch && fileNameMatch.length > 1 ? fileNameMatch[1] : undefined;
+            }
+            return response.blob().then(blob => { return { fileName: fileName, data: blob, status: status, headers: _headers }; });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<FileResponse>(null as any);
+    }
 }
 
 export class PriceClient {
@@ -959,6 +1034,7 @@ export interface UpdatePladeRequest {
     pladeId?: string;
     gentag?: boolean;
     betalt?: boolean;
+    udbetalt?: boolean;
 }
 
 export interface AdminPladeResponse {
@@ -967,6 +1043,14 @@ export interface AdminPladeResponse {
     transaktionsNr?: string;
     pris?: number;
     betalt?: boolean;
+}
+
+export interface WinningPladeResponse {
+    pladeId?: string;
+    brugernavn?: string;
+    transaktionsNr?: string;
+    pris?: number;
+    udbetalt?: boolean;
 }
 
 export interface PriceResponse {
